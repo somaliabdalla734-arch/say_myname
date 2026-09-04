dbcf716939b3b04d420aba84cfc0ffe473d4cd85
eng.abdalla <somaliabdalla734@gmail.com>
2026-09-05 01:48:02 +0300

    Fix port for Render

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
